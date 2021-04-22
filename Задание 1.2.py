6c49fc24e82ce805d75ac6784a5c2e8364359203
Usalina <usalina9@mail.ru>
2021-04-22 21:47:39 +0900

    initial commit

--- Задание 1.2.py
+++
@@ -1,7 +0,0 @@
-number=int(input('Введите время в секундах: '))
-
-hour=int(number//3600)
-min=int(number%3600//60)
-sec=int(number%60)
-
-print(f"{hour:02}:{min:02}:{sec:02}")
--- Задание 1.3.py
+++
@@ -1,5 +0,0 @@
-number=input('Введите число ')
-
-result=(int(number)+int((number+number))+int((number+number+number)))
-
-print(result)
