d0c16142a15cdb3834516e3d17fd82f09dfa9169
Usalina <usalina9@mail.ru>
2021-04-22 22:54:22 +0900

    initial commit

--- Задание 1.4.py
+++ Задание 1.4.py
@@ -12,17 +12,6 @@ length=int(len(number))
 
 list_n=list(number)
 
-<<<<<<< HEAD
-while length>1:
-    if list_n[0:2]<=list_n[1:3]:
-        del(list_n[0:2])
-    elif list_n[1:3]<=list_n[0:2]:
-        del(list_n[1:3])
-print(list_n)
-
-не могу понять что не так
-=======
->>>>>>> 0a2e55e (initial commit)
 
 
 
